0165e87896a5ac62892501d947404685dd5befa3
Jan Henckens <jan@statik.be>
2022-10-10 15:32:35 +0200

    Fixed settings labels + translations
    Signed-off-by: Jan Henckens <jan@statik.be>

--- src/translations/nl/deepl.php
+++ src/translations/nl/deepl.php
@@ -1,7 +1,10 @@
 <?php
 
 return [
-    "Entries in the primary site are not allowed to be translated. Contact support for further questions." => "Pagina\'s uit de hoofd-site kunnen niet vertaald worden. Contacteer support voor meer informatie.",
+    "Entries in the primary site are not allowed to be translated. Contact support for further questions." => "Pagina\'s uit de primaire site kunnen niet vertaald worden. Contacteer support voor meer informatie.",
     "Translate this page with content from:" => "Vertaal deze pagina met inhoud uit:",
     "From {locale}" => "Vanuit het {locale}",
+    "DeepL API key" => "DeepL API sleutel",
+    "Your API key can be found in your [DeepL Account](https://www.deepl.com/account/summary)" => "Je kan je API sleutel vinden in de je [DeepL account](https://www.deepl.com/account/summary)",
+    "Allow entries in the primary site to be translated" => "Laat toe dat entries uit de primaire site vertaald kunnen worden"
 ];
