bf62c416ed3e49da281ed4a5a95043b44b49828d
Jan Henckens <jan@statik.be>
2022-10-10 13:40:09 +0200

    Better labels
    Signed-off-by: Jan Henckens <jan@statik.be>

---
+++ src/translations/nl/deepl.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    "Entries in the primary site are not allowed to be translated. Contact support for further questions." => "Pagina\'s uit de hoofd-site kunnen niet vertaald worden. Contacteer support voor meer informatie.",
+    "Translate this page with content from:" => "Vertaal deze pagina met inhoud uit:",
+    "From {locale}" => "Vanuit het {locale}",
+];
